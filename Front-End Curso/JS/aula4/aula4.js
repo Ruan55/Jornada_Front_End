8070b55bf026265c6bb3824957970f9a190380ae
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2025-09-08 13:06:51 -0300

    Add files via upload

---
+++ Front-End Curso/JS/aula4/aula4.js
@@ -0,0 +1,26 @@
+let operacao1 = 5 + 2 // Versão alternativa: 5 += 2
+
+let operacao2 = 5 - 2  // Versão alternativa: 5 -= 2 
+
+let operacao3 = 5 * 2 // Versão alternativa: 5 *= 2
+
+let operacao4 = 5 / 2 // Versão alternativa: 5 /= 2
+
+let operacao5 = 5 % 2 // Versão alternativa: 5 %= 2
+
+let operacao6 = 5 ** 2 // Versão alternativa: 5 **= 2
+
+let operacao7 = (5 + 3) / 2 
+
+console.log(operacao7)
+
+/*
+
+    ORDEM DE PRECEDÊNCIA:
+
+    1 - ()
+    2 - **
+    3 - *, /, %
+    4 - +, -
+
+*/
---
+++ Front-End Curso/JS/aula5/aula5.js
@@ -0,0 +1,49 @@
+/*
+
+Operadores Relacionais
+
+5 > 2 Maior que
+
+7 < 4 Menor que
+
+8 >= 8 Maior igual que
+
+9 >= 7 Menor igual que
+
+5 == '5' Igual
+
+5 === '5' Idêntico
+
+4 != 4 Diferente
+
+4 !== '4' Desigual
+
+-----------------------------------
+
+Operadores Lógicos
+
+! = NOT
+
+&& = AND
+
+|| = OR
+
+-----------------------------------
+
+Operador Ternário
+
+? = questionamento
+
+: = situação
+
+-----------------------------------
+
+Precedência
+
+1 - (), **, / ...
+2 - >, <, >= ...
+3 - !
+4 - &&
+5 - ||
+
+*/

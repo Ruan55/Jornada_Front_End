4cf96a45e936337187d36290859348a1ce91f053
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2025-09-22 12:56:21 -0300

    Add files via upload

---
+++ Front-End Curso/JS/aula12/aula12.js
@@ -0,0 +1,31 @@
+// Variável que armazena os elementos em um array
+let num = [5, 8, 2, 9, 3];
+
+// Adiciona um elemento manualmente
+// num[5] = 10;
+
+// Adiciona um elemento automaticamente
+// num.push(55); 
+
+// Imprimindo o tamanho do array
+// console.log(num.length);
+
+// Sorteia a ordem dos elementos de forma aleatória
+// num.sort();
+
+// Imprime o valor selecionado do array manualmente
+// console.log(num[3]);
+
+// Tratamento de Array com estrutura de repetição For
+// for(let pos = 0; pos < num.length; pos++){
+
+//     console.log(num[pos]);
+
+// }
+
+// Tratamento de Array com estrutura de repetição For in
+for(let pos in num){
+
+    console.log(num[pos]);
+
+}

6a9145d7287936c7f2a2c775e8777a0dafbe4d4f
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2025-09-23 17:40:05 -0300

    Add files via upload

---
+++ Front-End Curso/JS/aula13/aula13.js
@@ -0,0 +1,19 @@
+function parOuImpar(n){
+
+    if(n % 2 == 0){
+
+        console.log(`${n} é um número par`);
+
+    }
+
+    else{
+
+        console.log(`${n} Este número é impar`);
+
+    }
+
+    return n;
+
+}
+
+let resultado = parOuImpar(92315);
